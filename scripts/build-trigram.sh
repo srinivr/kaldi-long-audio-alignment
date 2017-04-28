a92da859c17cfc87e66b43b9a1016352702d2832
Srinivas <srinivas.vr.edu@gmail.com>
2017-04-28 19:45:55 +0530

    removed $LMBIN

--- scripts/build-trigram.sh
+++ scripts/build-trigram.sh
@@ -6,7 +6,7 @@
 . ./longaudio_vars.sh
 working_dir=$1
 input_file=$2
-$LMBIN/build-lm.sh -i $input_file -o $working_dir/lm.gz -n 3
-$LMBIN/compile-lm $working_dir/lm.gz -t=yes /dev/stdout | grep -v unk | gzip -c > $working_dir/lm.arpa.gz
+build-lm.sh -i $input_file -o $working_dir/lm.gz -n 3
+compile-lm $working_dir/lm.gz -t=yes /dev/stdout | grep -v unk | gzip -c > $working_dir/lm.arpa.gz
 gunzip -c $working_dir/lm.arpa.gz | arpa2fst --disambig-symbol=#0 --read-symbol-table=$lang_dir/words.txt - $lang_dir/G.fst
 
